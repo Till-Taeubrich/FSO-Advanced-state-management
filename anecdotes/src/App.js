7608dfb4d3f2472325e3e99b5ced3c7620348931
Till Taeubrich <till.taeubrich@gmail.com>
2023-07-31 18:33:14 +0200

    finish exercise 6.14

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react'
 import AnectodeForm from './components/AnectodeForm'
 import AnecdoteList from './components/AnectodeList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anectodeService from './services/anectodeService'
+import { setAnectodes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anectodeService.getAll().then((res) => {
+      dispatch(setAnectodes(res))
+    })
+  }, [dispatch])
 
   return (
     <div>
